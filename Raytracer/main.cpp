751deb4bc125466652026f9b81b2d6d00ab75f56
Hidde Lycklama <hidde@vlinderstorm.com>
2015-06-09 14:05:17 +0200

    Initial Commit

---
+++ Raytracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raytracer
+//
+//  Created by Hidde Lycklama on 6/9/15.
+//  Copyright (c) 2015 Hidde Lycklama. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
