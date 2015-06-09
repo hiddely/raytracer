81b5a1c3acb9a02564d2c555dd77364cc684f9f5
Hidde Lycklama <hidde@vlinderstorm.com>
2015-06-09 15:25:31 +0200

    Add example paths file

---
+++ Raytracer/paths.h.example.h
@@ -0,0 +1,15 @@
+//
+//  paths.h
+//  Raytracer
+//
+//  Created by Hidde Lycklama on 6/9/15.
+//  Copyright (c) 2015 Hidde Lycklama. All rights reserved.
+//
+
+#ifndef Raytracer_paths_h
+#define Raytracer_paths_h
+
+// UPDATE THIS LINE TO YOUR OWN PATH
+#define MESH_PATH "/Users/hidde/Desktop/raytracer/Raytracer/Raytracer/dodgeColorTest.obj"
+
+#endif
