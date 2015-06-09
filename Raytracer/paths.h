c274847fa06b38253b404266ad34d5a0c70e39a2
Hidde Lycklama <hidde@vlinderstorm.com>
2015-06-09 14:23:56 +0200

    add gitignore

---
+++ Raytracer/paths.h
@@ -0,0 +1,13 @@
+//
+//  paths.h
+//  Raytracer
+//
+//  Created by Hidde Lycklama on 6/9/15.
+//  Copyright (c) 2015 Hidde Lycklama. All rights reserved.
+//
+
+#ifndef Raytracer_paths_h
+#define Raytracer_paths_h
+
+
+#endif
