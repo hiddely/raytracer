6fb707670e64e484dd184ca9cea65ab852395cf9
Hidde Lycklama <hidde@vlinderstorm.com>
2015-06-16 15:51:54 +0200

    Add example paths file

---
+++ Raytracer/paths.h.example.h
@@ -0,0 +1,16 @@
+//
+//  paths.h
+//  Raytracer
+//
+//  Created by Hidde Lycklama on 6/9/15.
+//  Copyright (c) 2015 Hidde Lycklama. All rights reserved.
+//
+
+#ifndef Raytracer_paths_h
+#define Raytracer_paths_h
+
+// UPDATE THIS LINE TO YOUR OWN PATH
+#define MESH_PATH "/Users/hidde/Desktop/raytracer/Raytracer/Raytracer/cube.obj"
+#define IMAGE_PATH "/Users/hidde/Desktop/result.png"
+
+#endif
